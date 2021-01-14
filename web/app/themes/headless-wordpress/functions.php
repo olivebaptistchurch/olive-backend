9ec654fe99218bd684fcaf2d73ab3761ec352f7a
Colton Plumley <cplumley@olivebaptist.org>
2021-01-13 20:57:53 -0600

    Update list of plugins

--- web/app/themes/headless-wordpress/functions.php
+++ web/app/themes/headless-wordpress/functions.php
@@ -74,17 +74,17 @@ add_filter("acf/format_value", "acf_nullify_empty", 100, 3);
 // Remove menu Items
 function wpdocs_remove_menus(){
    
-  remove_menu_page( 'index.php' );                          //Dashboard
-  remove_menu_page( 'edit.php' );                           //Posts
-  remove_menu_page( 'upload.php' );                         //Media
-  remove_menu_page( 'edit.php?post_type=page' );            //Pages
-  remove_menu_page( 'edit-comments.php' );                  //Comments
-  remove_menu_page( 'themes.php' );                         //Appearance
-  remove_menu_page( 'plugins.php' );                        //Plugins
-  remove_menu_page( 'users.php' );                          //Users
-  remove_menu_page( 'tools.php' );                          //Tools
-  remove_menu_page( 'options-general.php' );                //Settings
-  remove_menu_page( 'edit.php?post_type=acf-field-group' ); //ACF
+//   remove_menu_page( 'index.php' );                          //Dashboard
+//   remove_menu_page( 'edit.php' );                           //Posts
+//   remove_menu_page( 'upload.php' );                         //Media
+//   remove_menu_page( 'edit.php?post_type=page' );            //Pages
+//   remove_menu_page( 'edit-comments.php' );                  //Comments
+//   remove_menu_page( 'themes.php' );                         //Appearance
+//   remove_menu_page( 'plugins.php' );                        //Plugins
+//   remove_menu_page( 'users.php' );                          //Users
+//   remove_menu_page( 'tools.php' );                          //Tools
+//   remove_menu_page( 'options-general.php' );                //Settings
+//   remove_menu_page( 'edit.php?post_type=acf-field-group' ); //ACF
    
 }
 add_action( 'admin_menu', 'wpdocs_remove_menus' );
