b9cd6f473f174019913142e3d43ec5fb93d5db14
Colton Plumley <cplumley@olivebaptist.org>
2021-01-14 23:09:48 -0600

    Add thumbnail upload with url and hidden input field

--- web/app/themes/headless-wordpress/js/admin-funeral.js
+++ web/app/themes/headless-wordpress/js/admin-funeral.js
@@ -1,22 +1,38 @@
 /*
   JavaScript code to add video URL to input on file upload
 */
+console.log('test');
 
 jQuery(document).ready(function() {
-  jQuery('body').on('DOMSubtreeModified', '.show-if-value', function() {
+  const videoUrlElement = document.querySelector('.acf-field-url[data-name="funeral_video_url"');
+  const thumbnailUrlElement = document.querySelector('.acf-field-url[data-name="thumbnail_url"');
+  thumbnailUrlElement.style.display = "none";
+
+  jQuery('body').on('DOMSubtreeModified', '.show-if-value.file-wrap', function() {
       var videoFileUrl = document.querySelector('.file-info a').href;
-      var target = document.querySelector('.acf-field-url input');
+      var target = videoUrlElement.querySelector('input');
 
       if( videoFileUrl ) {
         target.value = videoFileUrl;
       }
   });
 
+  document.querySelector('body').addEventListener("click", function() {
+    var imageFileUrl = document.querySelector('.show-if-value.image-wrap img').src;
+    var target = thumbnailUrlElement.querySelector('input');
+
+    if( imageFileUrl.startsWith("https://olivewebserver")  ) {
+      target.value = imageFileUrl;
+    } else {
+      target.value = '';
+    }
+  })
+  
+
   const cancelFile = document.querySelector('.show-if-value .-cancel')
 
   cancelFile.addEventListener("click", function() {
-    var videoFileUrl = document.querySelector('.file-info a').href;
-    var target = document.querySelector('.acf-field-url input');
+    var target = videoUrlElement.querySelector('input');
     
     // For whatever reason, this needs to be delayed
     setTimeout(function() {
